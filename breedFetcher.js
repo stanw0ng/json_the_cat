57538e74eae070e0fece321c42999a66b897def7
stanw0ng <user.stanley@gmail.com>
2023-03-16 15:17:35 -0400

    refactored breedfetcher.js and added index.js to reference it

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,36 @@
 const request = require('request');
 
-const urlSearch = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const breed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  const urlSearch = 'https://api.thecatapi.com/v1/breeds/search?q=';
+  const url = urlSearch + breedName;
 
-const url = urlSearch + breed; // get URL from command line argument
+  request(url, (error, response, body) => {
+    
+    //failures from
+    if (error) {
+      callback(error, null); // if error, invoke the callback where the error param is the error produced by request() and description is null
+      return;
+    }
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+    if (response.statusCode !== 200) {
+      const errorMsg = `Invalid status code: ${response.statusCode}`;
+      callback(errorMsg, null); // if error, invoke the callback where the error param is the errorMsg and description is null
+      return;
+    }
 
-  if (response.statusCode !== 200) {
-    console.error(`Invalid status code: ${response.statusCode}`);
-    return;
-  }
+    // successful request
+    const data = JSON.parse(body);
 
-  // converts body (previously a string) into a JSON object and stores it as a variable called data
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
+    // successful request BUT no such breed found through search
+    if (data.length === 0) {
+      const errorMsg = `${breedName} not found!`;
+      callback(errorMsg, null); // if error (but still successful request), invoke the callback where the error param is the errorMsg and description is null
+      return;
+    }
 
-  if (data.length === 0) {
-    console.error(`Error: ${breed} not found!`);
-    return;
-  }
-  
-  // otherwise access the description of the breed
-  console.log(data[0].description);
+    const breedDescription = data[0].description;
+    callback(null, breedDescription); // if request is succesful AND search has match, invoke the callback where the error param is null and description is breedDescription
+  });
+};
 
-});
+module.exports = {fetchBreedDescription}
